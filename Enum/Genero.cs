3fa143b4bdc9ac823f7f4e9c7972038a62ddcbd1
Lima <d.costalima@hotmail.com>
2021-05-05 16:31:08 -0400

    commit inicial

---
+++ Enum/Genero.cs
@@ -0,0 +1,19 @@
+namespace DIO.Series
+{
+	public enum Genero
+	{
+		Acao_desenfreada = 1,
+		Aventuras = 2,
+		Comedia_para_morrer_de_rir = 3,
+		Documentario = 4,
+		Dramas_para_chorar = 5,
+		Espionagem = 6,
+		Faroeste = 7,
+		Fantasia = 8,
+		Ficcao_Cientifica = 9,
+		Musical = 10,
+		Romance = 11,
+		Suspenses = 12,
+		Terror_para_morrer_de_medo = 13
+	}
+}
